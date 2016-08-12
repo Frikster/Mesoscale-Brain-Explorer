95d1b7abf509d0dd5d58be70ae4a66509f93e291
Kristian <kris34697@gmail.com>
2016-08-12 16:10:47 +0200

    Fix IndexError on ROI creation

---
+++ src/plugins/util/debug.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from PyQt4.QtCore import pyqtRemoveInputHook, pyqtRestoreInputHook
+from pdb import set_trace
+import sys
+import pdb
+
+class SetTrace:
+  def __init__(self):
+    pyqtRemoveInputHook()
+    set_trace()
+
+  def __del__(self):
+    pyqtRestoreInputHook()
+
+def set_trace():
+    pyqtRemoveInputHook()
+    # set up the debugger
+    debugger = pdb.Pdb()
+    debugger.reset()
+    # custom next to get outside of function scope
+    debugger.do_next(None) # run the next command
+    users_frame = sys._getframe().f_back # frame where the user invoked `pyqt_set_trace()`
+    debugger.interaction(users_frame, None)
