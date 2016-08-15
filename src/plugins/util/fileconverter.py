e9dfc029809d0b301d97b624f38f766da16d3e76
Kristian <kris34697@gmail.com>
2016-08-15 07:05:05 +0200

    Even better importing :)

--- src/plugins/util/fileconverter.py
+++ src/plugins/util/fileconverter.py
@@ -5,13 +5,31 @@ import numpy as np
 
 import tifffile as tiff
 
+class ConvertError(Exception):
+  pass
+
 def tif2npy(filename_from, filename_to, progress_callback):
-  assert(not os.path.isfile(filename_to))
   with tiff.TiffFile(filename_from) as tif:
     w, h = tif[0].shape
     shape = len(tif), w, h
     np.save(filename_to, np.empty(shape, tif[0].dtype))
     fp = np.load(filename_to, mmap_mode='r+')
     for i, page in enumerate(tif):
-      progress_callback(int(100.0 * i / float(shape[0]-1)))
+      progress_callback(i / float(shape[0]-1))
       fp[i] = page.asarray()
+
+def raw2npy(filename_from, filename_to, dtype, width, height,
+  num_channels, progress_callback):
+    fp = np.memmap(filename_from, dtype, 'r')
+    frame_size = width * height * num_channels
+    if len(fp) % frame_size:
+      raise ConvertError()
+    num_frames = len(fp) / frame_size
+    fp = np.memmap(filename_from, dtype, 'r',
+      shape=(num_frames, width, height, num_channels))
+    np.save(filename_to, np.empty((num_frames, width, height), dtype))
+    fp_to = np.load(filename_to, mmap_mode='r+')
+    for i, frame in enumerate(fp):
+      progress_callback(i / float(len(fp)-1))
+      fp_to[i] = frame[:,:,1]
+
