4e0fe97b8b05690c8f7df657d03fef7818f74175
Cornelis Dirk Haupt <dirk.haupt@gmail.com>
2017-01-04 15:28:02 -0800

    Added constants file; exporting type user-defined now

--- src/plugins/fileexporter.py
+++ src/plugins/fileexporter.py
@@ -7,6 +7,11 @@ import numpy as np
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+from .util import constants
+from .util import project_functions as pfs
+from .util.mygraphicsview import MyGraphicsView
+from .util.qt import MyListView
+
 sys.path.append('..')
 import qtutil
 
@@ -23,13 +28,13 @@ class Exporter(QWidget):
   def export_mp4(self, fileinfo, filename):
     pass
   
-  def export_avi(self, fileinfo, filename):
+  def export_avi(self, filepath, filename):
     progress = MyProgressDialog('File Exporter', 'Writing to avi...', self)
     try:
-      frames = fileloader.load_file(fileinfo['path'])
+      frames = fileloader.load_file(filepath)
       if frames.dtype != np.uint8:
         frames = frames.astype(np.uint8)
-      #todo: 30 = export framerate needs to be user-decided
+      # todo: 30 = export framerate needs to be user-decided
       video = cv2.VideoWriter(
         filename, cv2.VideoWriter_fourcc(*'DIVX'), 30,
         (frames.shape[1], frames.shape[2]), False
@@ -43,15 +48,15 @@ class Exporter(QWidget):
       progress.close()
       qtutil.critical('Video file could not be written.', self)
   
-  def export_tif(self, fileinfo, filename):
-    frames = fileloader.load_file(fileinfo['path'])
+  def export_tif(self, filepath, filename, export_dtype):
+    frames = fileloader.load_file(filepath)
     #todo: include option in export to select dtype (only 16 and 32 work in imageJ)
-    frames = frames.astype(np.float32)
+    frames = frames.astype(export_dtype)
     tiff.imsave(filename, frames)
   
-  def export_raw(self, fileinfo, filename):
-    frames = fileloader.load_file(fileinfo['path'])
-    frames = frames.astype(np.float32)
+  def export_raw(self, filepath, filename, export_dtype):
+    frames = fileloader.load_file(filepath)
+    frames = frames.astype(export_dtype)
     frames.tofile(filename)
   
   def export_roi(self, fileinfo, filename):
@@ -66,27 +71,90 @@ class NoFileSelectedError(Exception):
 class Widget(QWidget):
   def __init__(self, project, parent=None):
     super(Widget, self).__init__(parent)
-
     if not project:
       return
+    self.project = project
+
+    # define ui components and global data
+    self.view = MyGraphicsView(self.project)
+    self.video_list = MyListView()
+    self.left = QFrame()
+    self.right = QFrame()
+    self.left_cut_off = QSpinBox()
+    self.right_cut_off = QSpinBox()
 
     self.project = project
     self.setup_ui()
+    self.open_dialogs = []
+    self.selected_videos = []
+    self.shown_video_path = None
     self.exporter = Exporter(self)
 
+    self.video_list.setModel(QStandardItemModel())
+    self.video_list.selectionModel().selectionChanged.connect(self.selected_video_changed)
+    self.video_list.doubleClicked.connect(self.video_triggered)
+    for f in project.files:
+        if f['type'] != 'video':
+            continue
+        self.video_list.model().appendRow(QStandardItem(f['name']))
+    self.video_list.setCurrentIndex(self.video_list.model().index(0, 0))
+
+  def video_triggered(self, index):
+      pfs.video_triggered(self, index)
+
   def setup_ui(self):
-    vbox = QVBoxLayout()
+      vbox_view = QVBoxLayout()
+      vbox_view.addWidget(self.view)
+      self.left.setLayout(vbox_view)
 
-    self.table = FileTable(self.project.files)
-    self.table.setSelectionMode(QAbstractItemView.SingleSelection)
-    self.table.doubleClicked.connect(self.row_doubleclicked)
-    vbox.addWidget(self.table)
+      vbox = QVBoxLayout()
+      list_of_manips = pfs.get_list_of_project_manips(self.project)
+      self.toolbutton = pfs.add_combo_dropdown(self, list_of_manips)
+      self.toolbutton.activated.connect(self.refresh_video_list_via_combo_box)
+      vbox.addWidget(self.toolbutton)
+      vbox.addWidget(QLabel('Choose video:'))
+      # todo: include bulk export option
+      # self.video_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
+      self.video_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
+      # self.video_list.setStyleSheet('QListView::item { height: 26px; }')
+      vbox.addWidget(self.video_list)
 
-    pb = QPushButton('&Export')
-    pb.clicked.connect(self.export_clicked)
-    vbox.addWidget(pb)
+      self.cb_dtype = QComboBox()
+      for t in constants.DTYPES:
+          self.cb_dtype.addItem(t)
+      vbox.addWidget(self.cb_dtype)
 
-    self.setLayout(vbox)
+      pb = QPushButton('Export')
+      pb.clicked.connect(self.export_clicked)
+      vbox.addWidget(pb)
+      self.right.setLayout(vbox)
+
+      splitter = QSplitter(Qt.Horizontal)
+      splitter.setHandleWidth(3)
+      splitter.setStyleSheet('QSplitter::handle {background: #cccccc;}')
+      splitter.addWidget(self.left)
+      splitter.addWidget(self.right)
+      hbox_global = QHBoxLayout()
+      hbox_global.addWidget(splitter)
+      self.setLayout(hbox_global)
+    # vbox = QVBoxLayout()
+    #
+    # self.table = FileTable(self.project.files)
+    # self.table.setSelectionMode(QAbstractItemView.SingleSelection)
+    # self.table.doubleClicked.connect(self.row_doubleclicked)
+    # vbox.addWidget(self.table)
+    #
+    # pb = QPushButton('&Export')
+    # pb.clicked.connect(self.export_clicked)
+    # vbox.addWidget(pb)
+    #
+    # self.setLayout(vbox)
+
+  def refresh_video_list_via_combo_box(self, trigger_item=None):
+      pfs.refresh_video_list_via_combo_box(self, trigger_item)
+
+  def selected_video_changed(self, selected, deselected):
+      pfs.selected_video_changed_multi(self, selected, deselected)
 
   def filedialog(self, name, filters):
     path = QSettings().value('export_path')
@@ -119,7 +187,7 @@ class Widget(QWidget):
       assert(filename.endswith('.roi'))
       self.exporter.export_roi(fileinfo, filename)
 
-  def export_video(self, fileinfo):
+  def export_video(self, filepath):
     """Ask for filename and dispatch based on requested file format"""
     filters = {
       #'.mp4': 'MP4 file (*.mp4)',
@@ -127,44 +195,48 @@ class Widget(QWidget):
       '.tif': 'TIF file (*.tif)',
       '.raw': 'RAW file (*.raw)'
     }
-    default = 'name' in fileinfo and fileinfo['name'] or 'untitled'
+    default = os.path.basename(filepath) or 'untitled'
+    # default = 'name' in fileinfo and fileinfo['name'] or 'untitled'
     filename = self.filedialog(default, filters)
     if not filename:
       return
+    export_dtype = self.cb_dtype.currentText()
 
     if filename.endswith('.avi'):
-      self.exporter.export_avi(fileinfo, filename)
+      self.exporter.export_avi(filepath, filename)
     elif filename.endswith('.mp4'):
-      self.exporter.export_mp4(fileinfo, filename)
+      self.exporter.export_mp4(filepath, filename)
     elif filename.endswith('.tif'):
-      self.exporter.export_tif(fileinfo, filename)
+      self.exporter.export_tif(filepath, filename, export_dtype)
     elif filename.endswith('.raw'):
-      self.exporter.export_raw(fileinfo, filename)
+      self.exporter.export_raw(filepath, filename, export_dtype)
     else:
       assert(False)
 
-  def export(self, fileinfo):
-    """Dispatch according to field 'type'"""
-    if fileinfo['type'] == 'roi':
-      self.export_roi(fileinfo)
-    elif fileinfo['type'] == 'video':
-      self.export_video(fileinfo)
-    else:
-      qtutil.critical('Unsupported file type.')
+  def export(self, filepath):
+      self.export_video(filepath)
+    # """Dispatch according to field 'type'"""
+    # if fileinfo['type'] == 'roi':
+    #   self.export_roi(fileinfo)
+    # elif fileinfo['type'] == 'video':
+    #   self.export_video(filepath)
+    # else:
+    #   qtutil.critical('Unsupported file type.')
 
   def export_clicked(self):
     """Retrieve fileinfo and call export"""
-    rows = self.table.selectionModel().selectedRows()
-    if not rows:
-      return
-    assert(len(rows) == 1)
-    f = self.table.model().get_entry(rows[0])    
+    # rows = self.table.selectionModel().selectedRows()
+    # if not rows:
+    #   return
+    # assert(len(rows) == 1)
+    # f = self.table.model().get_entry(rows[0])
+    f = self.shown_video_path
     self.export(f)
 
-  def row_doubleclicked(self, index):
-    """Retrieve fileinfo and call export"""
-    f = self.table.model().get_entry(index)
-    self.export(f)
+  # def row_doubleclicked(self, index):
+  #   """Retrieve fileinfo and call export"""
+  #   f = self.table.model().get_entry(index)
+  #   self.export(f)
 
 class MyPlugin:
   def __init__(self, project):
---
+++ src/plugins/util/constants.py
@@ -0,0 +1,3 @@
+# Constants
+
+DTYPES = ['uint8', 'uint16', 'uint32','uint64', 'float16', 'float32', 'float64']
