689c71282c4a033c12b4ca4565fda6c71d51ed75
Cornelis Dirk Haupt <dirk.haupt@gmail.com>
2017-02-05 18:42:06 -0800

    ensure all selected plugins are ready for automation (unfinished)

--- src/pipegui.py
+++ src/pipegui.py
@@ -297,8 +297,19 @@ class MainWindow(QMainWindow):
 
       # order by index
       ordered_q_model_indexes = sorted(self.sidebar.pl_list.selectedIndexes(), key=lambda x: x.row(), reverse=False)
+      # ensure all selected plugins are ready for automation
+      for q_model_index in ordered_q_model_indexes:
+          p = self.plugins[q_model_index.data(Qt.UserRole)]
+          if not p.check_ready_for_automation():
+            qtutil.critical(p.automation_error_message())
+            return
+
       p = self.plugins[ordered_q_model_indexes[0].data(Qt.UserRole)]
       input_paths = p.get_input_paths()
+      if not input_paths:
+          qtutil.critical("The first plugin in the pipeline does not have a set of input files selected")
+          return
+
       for q_model_index in ordered_q_model_indexes:
           p = self.plugins[q_model_index.data(Qt.UserRole)]
           output_paths = p.run(input_paths)
