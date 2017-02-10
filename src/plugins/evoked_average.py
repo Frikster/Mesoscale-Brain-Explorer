a3a2867ef1b0aea5dd819f4b1e32d6dc6d29d438
Cornelis Dirk Haupt <dirk.haupt@gmail.com>
2017-02-10 01:51:59 -0800

    closes #24 - evoked_trigger automated and superclassed; however progress bars and mmap input still need to be implemented

--- src/plugins/evoked_average.py
+++ src/plugins/evoked_average.py
@@ -1,77 +1,117 @@
 #!/usr/bin/env python3
 
 import os
-import uuid
 
 import numpy as np
+import psutil
+import qtutil
 from PyQt4.QtGui import *
 
 from .util import fileloader
 from .util import project_functions as pfs
-from .util.qt import FileTable, FileTableModel
-import qtutil
+from .util.plugin import PluginDefault
+from .util.plugin import WidgetDefault
 
 
-class Widget(QWidget):
+class Widget(QWidget, WidgetDefault):
+    class Labels(WidgetDefault.Labels):
+        pass
+
+    class Defaults(WidgetDefault.Defaults):
+        manip = 'evoked-avg'
+
     def __init__(self, project, plugin_position, parent=None):
         super(Widget, self).__init__(parent)
-        if not project:
+        if not project or not isinstance(plugin_position, int):
             return
-        self.plugin_position = plugin_position
-        self.project = project
-        self.setup_ui()
-        self.update_tables()
+        self.avg_button = QPushButton('Generate Evoked Average')
+
+        # self.plugin_position = plugin_position
+        # self.project = project
+        # self.setup_ui()
+        # self.update_tables()
+        WidgetDefault.__init__(self, project, plugin_position)
 
     def setup_ui(self):
-        vbox = QVBoxLayout()
-        self.table1 = FileTable()
-        self.table1.setSelectionMode(QAbstractItemView.MultiSelection)
-        vbox.addWidget(self.table1)
-        pb = QPushButton('Generate Triggered Average')
-        pb.clicked.connect(self.trigger_average)
-        vbox.addWidget(pb)
-        self.table2 = FileTable()
-        vbox.addWidget(self.table2)
-        self.setLayout(vbox)
+        super().setup_ui()
+        # vbox = QVBoxLayout()
+        # self.table1 = FileTable()
+        # self.table1.setSelectionMode(QAbstractItemView.MultiSelection)
+        # vbox.addWidget(self.table1)
+        self.vbox.addWidget(self.avg_button)
+        # self.table2 = FileTable()
+        # self.vbox.addWidget(self.table2)
+        #self.setLayout(self.vbox)
 
-    def update_tables(self):
-        videos = [f for f in self.project.files if f['type'] == 'video']
-        self.table1.setModel(FileTableModel(videos))
-        videos = [
-            f for f in self.project.files
-            if 'manipulations' in f and 'trigger-avg' in f['manipulations']
-            ]
-        self.table2.setModel(FileTableModel(videos))
+    def setup_signals(self):
+        super().setup_signals()
+        self.avg_button.clicked.connect(self.execute_primary_function)
 
-    def trigger_average(self):
-        filenames = self.table1.selected_paths()
+
+    # def update_tables(self):
+    #     videos = [f for f in self.project.files if f['type'] == 'video']
+    #     self.table1.setModel(FileTableModel(videos))
+    #     videos = [
+    #         f for f in self.project.files
+    #         if 'manipulations' in f and 'trigger-avg' in f['manipulations']
+    #         ]
+    #     self.table2.setModel(FileTableModel(videos))
+
+    def execute_primary_function(self, input_paths=None):
+        if not input_paths:
+            if not self.selected_videos:
+                return
+            else:
+                selected_videos = self.selected_videos
+        else:
+            selected_videos = input_paths
+
+        filenames = selected_videos
         if len(filenames) < 2:
             qtutil.warning('Select multiple files to average.')
             return
-        frames = [fileloader.load_file(f) for f in filenames]
-        lens = [len(frames[x]) for x in range(len(frames))]
+        stacks = [fileloader.load_file(f) for f in filenames]
+        lens = [len(stacks[x]) for x in range(len(stacks))]
         min_lens = np.min(lens)
 
-        length = frames[0].shape[1]
-        breadth = frames[0].shape[2]
+        breadth = stacks[0].shape[1]
+        length = stacks[0].shape[2]
 
         trig_avg = np.empty([min_lens, length, breadth])
-        for frame_set_index in range(min_lens):
-            frames_to_avg = [frames[frame_index][frame_set_index]
-                             for frame_index in range(len(frames))]
-            frames_to_avg = np.concatenate(frames_to_avg)
+        for frame_index in range(min_lens):
+            frames_to_avg = [stacks[stack_index][frame_index]
+                             for stack_index in range(len(stacks))]
+            frames_to_avg = np.array(frames_to_avg)
             avg = np.mean(frames_to_avg, axis=0)
-            trig_avg[frame_set_index] = avg
-        pfs.save_project(os.path.join(self.project.path, str(uuid.uuid4())
-                                      , self.project, trig_avg, 'trigger-avg'),'video')
+            trig_avg[frame_index] = avg
+        manip = self.Defaults.manip + '_' + str(len(filenames))
+        output_path = pfs.save_project(filenames[0], self.project, trig_avg, manip, 'video')
+        pfs.refresh_list(self.project, self.video_list,
+                         self.params[self.Labels.video_list_indices_label],
+                         self.Defaults.list_display_type,
+                         self.params[self.Labels.last_manips_to_display_label])
+        return output_path
+        # self.update_tables()
 
-        self.update_tables()
 
-
-class MyPlugin:
+class MyPlugin(PluginDefault):
     def __init__(self, project, plugin_position):
         self.name = 'Evoked Average'
         self.widget = Widget(project, plugin_position)
+        super().__init__(self.widget, self.widget.Labels, self.name)
 
-    def run(self):
-        pass
+    # todo: memory map all input such that memory is only needed for the size of one
+    # todo: add progress bars
+    def check_ready_for_automation(self):
+        self.summed_filesize = 0
+        for path in self.widget.selected_videos:
+            self.summed_filesize = self.summed_filesize + os.path.getsize(path)
+        self.available = list(psutil.virtual_memory())[1]
+        if self.summed_filesize > self.available:
+            return False
+        return True
+
+    def automation_error_message(self):
+        return "Not enough memory. All files to be averaged together are of size ~"+str(self.summed_filesize) +\
+               " and available memory is: " + str(self.available)
+
