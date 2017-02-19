b59f4de7810c5e262d6bb6367b4c84b5f4be3312
Cornelis Dirk Haupt <dirk.haupt@gmail.com>
2017-02-18 17:28:31 -0800

    scatterplot fixed and whats this added

--- src/plugins/scatterplot.py
+++ src/plugins/scatterplot.py
@@ -58,9 +58,9 @@ w.addItem(sp2)
 ## and pxMode = False
 ##
 
-pos3 = np.zeros((100,100,3))
+pos3 = np.zeros((100, 100, 3))
 pos3[:,:,:2] = np.mgrid[:100, :100].transpose(1,2,0) * [-0.1,0.1]
-pos3 = pos3.reshape(10000,3)
+pos3 = pos3.reshape(10000, 3)
 d3 = (pos3**2).sum(axis=1)**0.5
 
 sp3 = gl.GLScatterPlotItem(pos=pos3, color=(1,1,1,.3), size=0.1, pxMode=False)
@@ -73,22 +73,22 @@ def update():
   global phase, sp2, d2
   s = -np.cos(d2*2+phase)
   color = np.empty((len(d2),4), dtype=np.float32)
-  color[:,3] = np.clip(s * 0.1, 0, 1)
-  color[:,0] = np.clip(s * 3.0, 0, 1)
-  color[:,1] = np.clip(s * 1.0, 0, 1)
-  color[:,2] = np.clip(s ** 3, 0, 1)
+  color[:, 3] = np.clip(s * 0.1, 0, 1)
+  color[:, 0] = np.clip(s * 3.0, 0, 1)
+  color[:, 1] = np.clip(s * 1.0, 0, 1)
+  color[:, 2] = np.clip(s ** 3, 0, 1)
   sp2.setData(color=color)
   phase -= 0.1
 
   ## update surface positions and colors
   global sp3, d3, pos3
   z = -np.cos(d3*2+phase)
-  pos3[:,2] = z
+  pos3[:, 2] = z
   color = np.empty((len(d3),4), dtype=np.float32)
-  color[:,3] = 0.3
-  color[:,0] = np.clip(z * 3.0, 0, 1)
-  color[:,1] = np.clip(z * 1.0, 0, 1)
-  color[:,2] = np.clip(z ** 3, 0, 1)
+  color[:, 3] = 0.3
+  color[:, 0] = np.clip(z * 3.0, 0, 1)
+  color[:, 1] = np.clip(z * 1.0, 0, 1)
+  color[:, 2] = np.clip(z ** 3, 0, 1)
   sp3.setData(pos=pos3, color=color)
   
 t = QtCore.QTimer()
@@ -96,8 +96,11 @@ t.timeout.connect(update)
 
 class MyPlugin:
   def __init__(self, project, plugin_position):
-    self.name = 'Scatterplot'
+    self.name = 'OpenGL Showcase'
     self.widget = w
+    self.widget.setWhatsThis("This is just a quick showcase that 3D visualization is possible for future developers "
+                             "looking to extend the application's functionality")
+    t.start(50)
 
   def run(self):
     t.start(50)
