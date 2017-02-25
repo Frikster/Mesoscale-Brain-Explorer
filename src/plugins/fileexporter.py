750ea46caac550167648e70597d098670f12037a
Cornelis Dirk Haupt <dirk.haupt@gmail.com>
2017-02-25 01:54:45 -0800

    bulk export works for avi

--- src/plugins/fileexporter.py
+++ src/plugins/fileexporter.py
@@ -20,6 +20,8 @@ from .util import fileloader
 
 import tifffile as tiff
 import cv2
+from .util.plugin import PluginDefault
+from .util.plugin import WidgetDefault
 
 class Exporter(QWidget):
   def __init__(self, parent=None):
@@ -64,78 +66,86 @@ class Exporter(QWidget):
     except:
       qtutil.critical('File could not be copied.\n' + traceback.format_exc(), self)
 
-class NoFileSelectedError(Exception):
-  pass
+class Widget(QWidget, WidgetDefault):
+  class Labels(WidgetDefault.Labels):
+    export_dtype_label = 'export_dtype_label'
+    export_framerate_label = 'Export Framerate'
 
-class Widget(QWidget):
-  def __init__(self, project, parent=None):
+  class Defaults(WidgetDefault.Defaults):
+    export_dtype_default = 0
+    export_framerate_default = 30
+
+  def __init__(self, project, plugin_position, parent=None):
     super(Widget, self).__init__(parent)
-    if not project:
-      return
+    if not project or not isinstance(plugin_position, int):
+        return
     self.project = project
 
     # define ui components and global data
-    self.view = MyGraphicsView(self.project)
-    self.video_list = MyListView()
-    self.left = QFrame()
-    self.right = QFrame()
-    self.left_cut_off = QSpinBox()
-    self.right_cut_off = QSpinBox()
-
-    self.project = project
-    self.setup_ui()
-    self.open_dialogs = []
-    self.selected_videos = []
-    self.shown_video_path = None
+    # self.view = MyGraphicsView(self.project)
+    # self.video_list = MyListView()
+    # self.left = QFrame()
+    # self.right = QFrame()
+    # self.left_cut_off = QSpinBox()
+    # self.right_cut_off = QSpinBox()
+    #
+    # self.project = project
+    # self.setup_ui()
+    # self.open_dialogs = []
+    # self.selected_videos = []
+    # self.shown_video_path = None
     self.exporter = Exporter(self)
+    self.export_pb = QPushButton('Export')
+    self.framerate_sb = QSpinBox()
+    self.cb_dtype = QComboBox()
 
-    self.video_list.setModel(QStandardItemModel())
-    self.video_list.selectionModel().selectionChanged.connect(self.selected_video_changed)
-    self.video_list.doubleClicked.connect(self.video_triggered)
-    for f in project.files:
-        if f['type'] != 'video':
-            continue
-        self.video_list.model().appendRow(QStandardItem(f['name']))
-    self.video_list.setCurrentIndex(self.video_list.model().index(0, 0))
+    # self.video_list.setModel(QStandardItemModel())
+    # self.video_list.selectionModel().selectionChanged.connect(self.selected_video_changed)
+    # self.video_list.doubleClicked.connect(self.video_triggered)
+    # for f in project.files:
+    #     if f['type'] != 'video':
+    #         continue
+    #     self.video_list.model().appendRow(QStandardItem(f['name']))
+    # self.video_list.setCurrentIndex(self.video_list.model().index(0, 0))
+    WidgetDefault.__init__(self, project, plugin_position)
 
-  def video_triggered(self, index):
-      pfs.video_triggered(self, index)
+  # def video_triggered(self, index):
+  #     pfs.video_triggered(self, index)
 
   def setup_ui(self):
-      vbox_view = QVBoxLayout()
-      vbox_view.addWidget(self.view)
-      self.left.setLayout(vbox_view)
-
-      vbox = QVBoxLayout()
-      list_of_manips = pfs.get_list_of_project_manips(self.project)
-      self.toolbutton = pfs.add_combo_dropdown(self, list_of_manips)
-      self.toolbutton.activated.connect(self.refresh_video_list_via_combo_box)
-      vbox.addWidget(self.toolbutton)
-      vbox.addWidget(QLabel('Choose video:'))
-      # todo: include bulk export option
-      # self.video_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
-      self.video_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
-      # self.video_list.setStyleSheet('QListView::item { height: 26px; }')
-      vbox.addWidget(self.video_list)
-
-      self.cb_dtype = QComboBox()
+      super().setup_ui()
+      # vbox_view = QVBoxLayout()
+      # vbox_view.addWidget(self.view)
+      # self.left.setLayout(vbox_view)
+      #
+      # vbox = QVBoxLayout()
+      # list_of_manips = pfs.get_list_of_project_manips(self.project)
+      # self.toolbutton = pfs.add_combo_dropdown(self, list_of_manips)
+      # self.toolbutton.activated.connect(self.refresh_video_list_via_combo_box)
+      # vbox.addWidget(self.toolbutton)
+      # vbox.addWidget(QLabel('Choose video:'))
+      # # self.video_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
+      # self.video_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
+      # # self.video_list.setStyleSheet('QListView::item { height: 26px; }')
+      # vbox.addWidget(self.video_list)
+      self.vbox.addWidget(QLabel("Export Framerate (Hz)"))
+      self.vbox.addWidget(self.framerate_sb)
+      self.framerate_sb.setMinimum(1)
+      self.framerate_sb.setMaximum(99999)
       for t in constants.DTYPES:
           self.cb_dtype.addItem(t)
-      vbox.addWidget(self.cb_dtype)
-
-      pb = QPushButton('Export')
-      pb.clicked.connect(self.export_clicked)
-      vbox.addWidget(pb)
-      self.right.setLayout(vbox)
-
-      splitter = QSplitter(Qt.Horizontal)
-      splitter.setHandleWidth(3)
-      splitter.setStyleSheet('QSplitter::handle {background: #cccccc;}')
-      splitter.addWidget(self.left)
-      splitter.addWidget(self.right)
-      hbox_global = QHBoxLayout()
-      hbox_global.addWidget(splitter)
-      self.setLayout(hbox_global)
+      self.vbox.addWidget(self.cb_dtype)
+      self.vbox.addWidget(self.export_pb)
+      # self.right.setLayout(self.vbox)
+      #
+      # splitter = QSplitter(Qt.Horizontal)
+      # splitter.setHandleWidth(3)
+      # splitter.setStyleSheet('QSplitter::handle {background: #cccccc;}')
+      # splitter.addWidget(self.left)
+      # splitter.addWidget(self.right)
+      # hbox_global = QHBoxLayout()
+      # hbox_global.addWidget(splitter)
+      # self.setLayout(hbox_global)
     # vbox = QVBoxLayout()
     #
     # self.table = FileTable(self.project.files)
@@ -149,11 +159,24 @@ class Widget(QWidget):
     #
     # self.setLayout(vbox)
 
-  def refresh_video_list_via_combo_box(self, trigger_item=None):
-      pfs.refresh_video_list_via_combo_box(self, trigger_item)
+  def setup_signals(self):
+      super().setup_signals()
+      self.export_pb.clicked.connect(self.export_clicked)
 
-  def selected_video_changed(self, selected, deselected):
-      pfs.selected_video_changed_multi(self, selected, deselected)
+  def setup_params(self, reset=False):
+      super().setup_params(reset)
+      if len(self.params) == 1 or reset:
+          self.update_plugin_params(self.Labels.export_framerate_label, self.Defaults.export_framerate_default)
+          self.update_plugin_params(self.Labels.export_dtype_label, self.Defaults.export_dtype_default)
+      self.framerate_sb.setValue(self.params[self.Labels.export_framerate_label])
+      self.cb_dtype.setCurrentIndex(self.params[self.Labels.export_dtype_label])
+
+
+  # def refresh_video_list_via_combo_box(self, trigger_item=None):
+  #     pfs.refresh_video_list_via_combo_box(self, trigger_item)
+  #
+  # def selected_video_changed(self, selected, deselected):
+  #     pfs.selected_video_changed_multi(self, selected, deselected)
 
   def filedialog(self, name, filters):
     path = QSettings().value('export_path')
@@ -229,18 +252,21 @@ class Widget(QWidget):
     #   return
     # assert(len(rows) == 1)
     # f = self.table.model().get_entry(rows[0])
-    f = self.shown_video_path
-    self.export(f)
+
+    #    f = self.shown_video_path
+    for selected_video in self.selected_videos:
+        self.export(selected_video)
 
   # def row_doubleclicked(self, index):
   #   """Retrieve fileinfo and call export"""
   #   f = self.table.model().get_entry(index)
   #   self.export(f)
 
-class MyPlugin:
+class MyPlugin(PluginDefault):
   def __init__(self, project, plugin_position):
     self.name = 'Export files'
-    self.widget = Widget(project)
+    self.widget = Widget(project, plugin_position)
+    super().__init__(self.widget, self.widget.Labels, self.name)
 
   def run(self):
     pass
