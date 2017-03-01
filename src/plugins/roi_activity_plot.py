14ca32e1d4f8a35bc708e3e5e50fe869b3dd0005
Cornelis Dirk Haupt <dirk.haupt@gmail.com>
2017-03-01 13:29:20 -0800

    readd comments for old plot activity method

--- src/plugins/roi_activity_plot.py
+++ src/plugins/roi_activity_plot.py
@@ -59,6 +59,230 @@ kelly_colors = [
   Color('dark_olive_green', (35, 44, 22))
 ]
 
+# def plot_roi_activities(video_path, rois, image, plot_title, win_title, progress_callback):
+#   frames = fileloader.load_file(video_path)
+#   #frames = np.swapaxes(np.swapaxes(frames, 0, 1), 1, 2)
+#   # plot_out = os.path.join(os.path.dirname(video_path), plot_title + str(uuid.uuid4()))
+#   plot_out = os.path.join(os.path.dirname(video_path), plot_title + '.csv')
+#
+#   roi_names = []
+#   ps = []
+#   for i, roi in enumerate(rois):
+#     progress_callback(i / len(rois))
+#     mask = roi.getROIMask(frames, image, axes=(1, 2))
+#     size = np.count_nonzero(mask)
+#     roi_frames = frames * mask[np.newaxis, :, :]
+#     roi_frames = np.ndarray.sum(np.ndarray.sum(roi_frames, axis=1), axis=1)
+#     p = roi_frames / size
+#     ps = ps + [p]
+#     roi_names = roi_names + [roi.name]
+#
+#   ps = [list(p) for p in ps]
+#   ps_rows = list(zip(*ps))
+#   plot_out = os.path.join(os.path.dirname(video_path), plot_title + '.csv')
+#   with open(plot_out, 'w', newline='') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=',',
+#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#     writer.writerow(roi_names)
+#     for i, row in enumerate(ps_rows):
+#         #progress_callback(i / len(rois))
+#         writer.writerow([str(p) for p in row])
+#   progress_callback(1)
+#
+#   win = pg.GraphicsWindow(title=win_title)
+#   win.resize(1000, 600)
+#   # win.setWindowTitle('Activity across frames')
+#   plot = win.addPlot(title=plot_title)
+#   plot.setLabel('bottom', "Image Frames")
+#   plot.setLabel('left', "Activity")
+#   plot.addLegend()
+#
+#   pg.setConfigOptions(antialias=True)
+#
+#   warning_issued = False
+#   for i, roi in enumerate(rois):
+#     # progress_callback(i / len(rois))
+#     mask = roi.getROIMask(frames, image, axes=(1, 2))
+#     size = np.count_nonzero(mask)
+#     roi_frames = frames * mask[np.newaxis, :, :]
+#     roi_frames = np.ndarray.sum(np.ndarray.sum(roi_frames, axis=1), axis=1)
+#     p = roi_frames / size
+#     if i < len(brewer_colors):
+#         color = brewer_colors[i].rgb
+#     else:
+#         if not warning_issued:
+#             qtutil.warning('Perceptual distinctiveness limit (12) reached. Resorting to Kelly colours. '
+#                            'Please be careful with how you interpret your data')
+#             warning_issued = True
+#         if i < len(brewer_colors) + len(kelly_colors):
+#             color = kelly_colors[i-len(brewer_colors)].rgb
+#         else:
+#             qtutil.critical('Colour limit reached. Please plot your data over multiple plots or output solely to csv')
+#             return win
+#     plot.plot(p, pen=color, name=roi.name)
+#   return win
+
+# def plot_rois_same_axis(video_paths, rois, image, progress_callback=None):
+#       if not video_paths:
+#           qtutil.critical("No video selected.")
+#           return
+#
+#       main_window = QMainWindow()
+#       area = DockArea()
+#       main_window.setCentralWidget(area)
+#       main_window.resize(2000, 900)
+#       main_window.setWindowTitle("Window ID - " + str(uuid.uuid4()) +
+#                                  ". Use Help -> What's This on this window for contextual tips")
+#
+#      # d1 = Dock("Save/Load", size=(1, 1))  ## give this dock the minimum possible size
+#       d2 = Dock("Dock2", size=(500, 200), closable=True)
+#       d3 = Dock("Dock3", size=(500, 200), closable=True)
+#       d4 = Dock("Dock4", size=(500, 200), closable=True)
+#       d5 = Dock("Dock5", size=(500, 200), closable=True)
+#       # area.addDock(d1, 'top')
+#       area.addDock(d2)
+#       area.addDock(d3, 'bottom', d2)
+#       area.addDock(d4, 'right', d3)
+#       area.addDock(d5, 'top', d4)
+#       area.moveDock(d5, 'right', d2)
+#
+#       # Code for adding a dock window which can save restore dock state. Uncomment to include
+#       ## first dock gets save/restore buttons
+#       # w1 = pg.LayoutWidget()
+#       # label = QLabel("""
+#       # This window has 4 Dock widgets in it. Each dock can be dragged
+#       # by its title bar to occupy a different space within the window
+#       # but note that one dock has its title bar hidden). Additionally,
+#       # the borders between docks may be dragged to resize. Docks that are dragged on top
+#       # of one another are stacked in a tabbed layout. Double-click a dock title
+#       # bar to place it in its own window.
+#       # """)
+#       # saveBtn = QPushButton('Save dock state')
+#       # restoreBtn = QPushButton('Restore dock state')
+#       # restoreBtn.setEnabled(False)
+#       # w1.addWidget(label, row=0, col=0)
+#       # w1.addWidget(saveBtn, row=1, col=0)
+#       # w1.addWidget(restoreBtn, row=2, col=0)
+#       # d1.addWidget(w1)
+#       # dock_state = None
+#       # def save():
+#       #     global dock_state
+#       #     dock_state = area.saveState()
+#       #     restoreBtn.setEnabled(True)
+#       # def load():
+#       #     global dock_state
+#       #     area.restoreState(dock_state)
+#       # saveBtn.clicked.connect(save)
+#       # restoreBtn.clicked.connect(load)
+#
+#       # Collect ROI activities for each selected vid
+#       progress_load = QProgressDialog('Loading files and collecting ROI data. This may take a while '
+#                                       'for large files.', 'Abort', 0, 100)
+#       progress_load.setAutoClose(True)
+#       progress_load.setMinimumDuration(0)
+#       def callback_load(x):
+#           progress_load.setValue(x * 100)
+#           QApplication.processEvents()
+#       video_path_to_plots_dict = {}
+#       for i, video_path in enumerate(video_paths):
+#           callback_load(i / len(video_paths))
+#           if progress_load.wasCanceled():
+#               return
+#           frames = fileloader.load_file(video_path)
+#           roi_activity_dict = {}
+#           # roi_names = []
+#           # ps = []
+#           for j, roi in enumerate(rois):
+#               mask = roi.getROIMask(frames, image, axes=(1, 2))
+#               size = np.count_nonzero(mask)
+#               if size <= 0:
+#                   qtutil.critical("No ROI selected. If you do have ROIs selected then the most likely cause is that "
+#                                   "you have modified the unit per pixel and not updated the size of the ROIs which "
+#                                   "don't adjust automatically. So for example any ROI with length 1 might now be less "
+#                                   "than 1 pixel after you changed the unit per pixel. To update the ROIs against your "
+#                                   "new unit per pixel, go to the import ROI plugin and press the 'Update Table' button")
+#                   return
+#               roi_frames = frames * mask[np.newaxis, :, :]
+#               roi_frames = np.ndarray.sum(np.ndarray.sum(roi_frames, axis=1), axis=1)
+#               p = roi_frames / size
+#               roi_activity_dict[roi.name] = p
+#               # ps = ps + [p]
+#               # roi_names = roi_names + [roi.name]
+#           video_path_to_plots_dict[video_path] = roi_activity_dict
+#       callback_load(1)
+#
+#       # regroup ROIs of same type together and add to Docks dynamically
+#       plot_docks = [0, 1, 2, 3]
+#       plot_docs_cycle = cycle(plot_docks)
+#       for roi in rois:
+#         warning_issued = False
+#         plots_for_roi = []
+#         source_names = []
+#         for video_path in video_path_to_plots_dict.keys():
+#             root, ext = os.path.splitext(video_path)
+#             source_name = os.path.basename(root)
+#             source_names = source_names + [source_name]
+#             plots_for_roi = plots_for_roi + [video_path_to_plots_dict[video_path][roi.name]]
+#
+#         # put all plots from one ROI on a single plot and place on one of the 4 docs
+#         next_dock = next(plot_docs_cycle)
+#         d = Dock(roi.name, size=(500, 200), closable=True)
+#         if next_dock == 0:
+#             area.addDock(d, 'above', d2)
+#         if next_dock == 1:
+#             area.addDock(d, 'above', d3)
+#         if next_dock == 2:
+#             area.addDock(d, 'above', d4)
+#         if next_dock == 3:
+#             area.addDock(d, 'above', d5)
+#
+#         doc_window = pg.GraphicsWindow(title="Dock plot")
+#         plot = doc_window.addPlot(title=roi.name)
+#         plot.setLabel('bottom', "Image Frames")
+#         plot.setLabel('left', "Activity")
+#         plot.addLegend()
+#         # w = pg.GraphicsWindow(title="Dock plot")
+#         # # w = pg.PlotWidget(title="Dock plot")
+#         # w.setLabel('bottom', "Image Frames")
+#         # w.setLabel('left', "Activity")
+#         # w.addLegend()
+#         assert(len(plots_for_roi) == len(source_names))
+#         for i, (p, source_name) in enumerate(zip(plots_for_roi, source_names)):
+#             if i < len(brewer_colors):
+#                 color = brewer_colors[i].rgb
+#             else:
+#                 if not warning_issued:
+#                     qtutil.warning('Perceptual distinctiveness limit (12) reached. Resorting to Kelly colours. '
+#                                    'Please be careful with how you interpret your data')
+#                     warning_issued = True
+#                 if i < len(brewer_colors) + len(kelly_colors):
+#                     color = kelly_colors[i - len(brewer_colors)].rgb
+#                 else:
+#                     qtutil.critical(
+#                         'Colour limit reached. Please plot your data over multiple plots or output solely to csv')
+#                     return doc_window
+#             plot.plot(p, pen=color, name=source_name)
+#         d.addWidget(doc_window)
+#
+#         # Save plot to file
+#         ps = [list(p) for p in plots_for_roi]
+#         ps_rows = list(zip(*ps))
+#         plot_out = os.path.join(os.path.dirname(video_paths[0]), roi.name + '_plots.csv')
+#         with open(plot_out, 'w', newline='') as csvfile:
+#             writer = csv.writer(csvfile, delimiter=',',
+#                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#             writer.writerow(source_names)
+#             for i, row in enumerate(ps_rows):
+#                 # progress_callback(i / len(rois))
+#                 writer.writerow([str(p) for p in row])
+#
+#       # close placeholder docks
+#       d2.close()
+#       d3.close()
+#       d4.close()
+#       d5.close()
+#       return main_window
+
 
 class Widget(QWidget, WidgetDefault):
   class Labels(WidgetDefault.Labels):
